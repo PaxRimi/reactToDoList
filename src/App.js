5c7bdc167810cf6112dcf128a2edcbfd770015ce
Wojciech Lewandowski <lewandowski.wojciech93@gmail.com>
2018-09-04 17:32:53 +0200

    Add method that add elements to the list

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import Todos from "./todolist";
+import AddElement from "./AddElementToDo";
 
 class App extends Component {
 
@@ -18,10 +19,18 @@ class App extends Component {
       });
     };
 
+    addToDo = element => {
+        element.id = this.state.todoes[this.state.todoes.length-1].id + 1;
+        this.setState({
+            todoes: [...this.state.todoes, element]
+        })
+    };
+
     render() {
     return (
       <div className="App container">
         <h1 className="center blue-text">Todo's</h1>
+          <AddElement addNewElement={this.addToDo}/>
           <Todos todo={this.state.todoes} deleteElement={this.deleteToDo}/>
       </div>
     );
