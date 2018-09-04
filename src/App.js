2d962a0fe703b115acfaba3de274cb412fdbc46c
Wojciech Lewandowski <lewandowski.wojciech93@gmail.com>
2018-09-04 16:56:26 +0200

    Add method that remove elements from the list

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import Todos from "./todolist";
 
 class App extends Component {
 
@@ -7,12 +8,21 @@ class App extends Component {
             { id: 1, content:"write some code"},
             { id: 2, content:"Work hard to be the best"}
         ]
-    }
+    };
+
+    deleteToDo = (id) => {
+      const toDoList = this.state.todoes.filter(element => {return element.id !== id});
+
+      this.setState({
+         todoes: toDoList,
+      });
+    };
 
     render() {
     return (
-      <div className="App">
-
+      <div className="App container">
+        <h1 className="center blue-text">Todo's</h1>
+          <Todos todo={this.state.todoes} deleteElement={this.deleteToDo}/>
       </div>
     );
   }
