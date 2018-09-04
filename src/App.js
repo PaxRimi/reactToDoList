6b8d0a27a628e156756dff42c9a67dd64eebe4a8
Wojciech Lewandowski <lewandowski.wojciech93@gmail.com>
2018-09-04 16:16:01 +0200

    Create starting component

--- src/App.js
+++ src/App.js
@@ -1,18 +1,18 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
 
 class App extends Component {
-  render() {
+
+    state = {
+        todoes: [
+            { id: 1, content:"write some code"},
+            { id: 2, content:"Work hard to be the best"}
+        ]
+    }
+
+    render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+
       </div>
     );
   }
